91339abf6fe688286147c88d4c3fe814e59f66f6
svdockum <stefanvandockum@gmail.com>
2026-03-30 09:19:15 +0200

    sidebar

--- src/components/header/SidebarV1.tsx
+++ src/components/header/SidebarV1.tsx
@@ -15,7 +15,7 @@ const SidebarV1 = ({ openSidebar, sidebarClose }: DataType) => {
                         <div className="logo-box">
                             <div className="nav-logo">
                                 <Link to="/">
-                                    <img src="/images/logo-2.svg" alt="image" />
+                                    <img src="/banner/avans.png" alt="image" />
                                 </Link>
                             </div>
                         </div>
